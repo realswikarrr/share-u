ae0d0d7068f258e0e54c93772033ed82b357983c
Swikar Adhikari <swikara082@gmail.com>
2022-07-28 14:28:23 +0545

    feat: ⚒️ Added Protection To Profile Route

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,31 @@
 import type { NextPage } from "next";
-import { useSession, signIn, signOut } from "next-auth/react";
+import { useSession, signIn, signOut, getSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   const { data: session } = useSession();
 
-  if (session) {
-    return (
-      <>
-        <div>Hello {session?.user?.email}</div>
-        <button onClick={() => signOut()}>Sign out</button>
-      </>
-    );
-  } else {
-    return (
-      <>
-        <div>Hello stranger</div>
-        <button onClick={() => signIn("google")}>SignIn</button>
-      </>
-    );
-  }
+  return (
+    <>
+      <div>Hello stranger</div>
+      <button onClick={() => signIn("google")}>SignIn</button>
+    </>
+  );
 };
 
 export default Home;
+
+export const getServerSideProps = async (ctx: any) => {
+  const session = await getSession(ctx);
+  if (session) {
+    return {
+      redirect: {
+        destination: "/profile",
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+};
--- pages/profile.tsx
+++ pages/profile.tsx
@@ -1,16 +1,39 @@
 import { trpc } from "../utils/trpc";
+import { useSession, signOut, getSession } from "next-auth/react";
 
 const Profile = () => {
+  const { data: session, status } = useSession();
   const hello = trpc.useQuery(["getLinks"]);
-  console.log(hello.data);
+
   if (!hello.data) {
     return <div>Loading...</div>;
   }
-  return (
-    <div>
-      <p>{hello.data[0]?.links[0]?.text}</p>
-    </div>
-  );
+
+  if (status === "authenticated") {
+    return (
+      <div>
+        <h1>Logged In As {session?.user?.email}</h1>
+        <p>{hello.data[0]?.links[0]?.text}</p>
+        <button onClick={() => signOut()}>Sign Out</button>
+      </div>
+    );
+  }
 };
 
 export default Profile;
+
+export const getServerSideProps = async (ctx: any) => {
+  const session = await getSession(ctx);
+  if (!session) {
+    return {
+      redirect: {
+        destination: "/",
+      },
+    };
+  }
+  return {
+    props: {
+      session,
+    },
+  };
+};
