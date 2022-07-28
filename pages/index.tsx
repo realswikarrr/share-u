ecdcae834758407c3bd71c25b9dbf9612e6dd450
Swikar Adhikari <swikara082@gmail.com>
2022-07-28 11:25:31 +0545

    feat: ⚒️ Tailwind Setup

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,7 @@ import type { NextPage } from "next";
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1 className="text-3xl">Hello World</h1>
     </div>
   );
 };
