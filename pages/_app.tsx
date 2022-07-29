adfa742ae0058cb940a070a100a712b2e3dd61c0
Swikar Adhikari <swikara082@gmail.com>
2022-07-29 15:55:43 +0545

    fix: 🐛 Some Deployment Bugs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,36 +14,19 @@ const MyApp: AppType = ({ Component, pageProps }) => {
 
 export default withTRPC<AppRouter>({
   config({ ctx }) {
-    if (typeof window !== "undefined") {
-      // during client requests
-      return {
-        transformer: superjson, // optional - adds superjson serialization
-        url: "/api/trpc",
-      };
-    }
-    // during SSR below
-
-    // optional: use SSG-caching for each rendered page (see caching section for more details)
-    const ONE_DAY_SECONDS = 60 * 60 * 24;
-    ctx?.res?.setHeader(
-      "Cache-Control",
-      `s-maxage=1, stale-while-revalidate=${ONE_DAY_SECONDS}`
-    );
-
-    // The server needs to know your app's full url
-    // On render.com you can use `http://${process.env.RENDER_INTERNAL_HOSTNAME}:${process.env.PORT}/api/trpc`
-    const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+    /**
+     * If you want to use SSR, you need to use the server's full URL
+     * @link https://trpc.io/docs/ssr
+     */
+    const url = "/api/trpc";
 
     return {
-      transformer: superjson, // optional - adds superjson serialization
       url,
-      headers: {
-        // optional - inform server that it's an ssr request
-        "x-ssr": "1",
-      },
+      transformer: superjson,
     };
   },
-  ssr: true,
+  /**
+   * @link https://trpc.io/docs/ssr
+   */
+  ssr: false,
 })(MyApp);
