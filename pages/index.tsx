8b10df0d14cb15fa4216bff7913e7c5644ee030b
realswikarrr <swikara082@gmail.com>
2022-08-03 20:01:54 +0545

    feat: 🔮 Added Styling To Home Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,27 @@ import { signIn, getSession } from "next-auth/react";
 
 const Home: NextPage = () => {
   return (
-    <>
-      <div>Hello stranger</div>
-    </>
+    <div className="container mx-auto text-center mt-5">
+      <h1 className="text-3xl">Hello And Welcome 🫣</h1>
+      <h1 className="mt-4 text-xl">
+        This Web App Helps You To Copy And Paste Links Across Multiple Devices{" "}
+        <br />
+        All You Have To Do Is Follow The Below Steps ⤵️
+      </h1>
+
+      <h1 className="mt-5 text-2xl">Steps To Follow 🕹️</h1>
+      <ul className="mt-2 bg-secondary p-2 text-center rounded-lg max-w-lg mx-auto">
+        <li className="text-white text-bold">
+          Step 1 : Sign Up Using Google Account ✨
+        </li>
+        <li className="mt-2 text-white text-bold">
+          Step 2 : Click On Get Started 🖱️{" "}
+        </li>
+        <li className="mt-2 text-white text-bold">
+          Step 3 : Enjoy Sharing Links 🤯{" "}
+        </li>
+      </ul>
+    </div>
   );
 };
 
