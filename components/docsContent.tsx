a1902eaa20af7192848cddb5af86489d98de3306
realswikarrr <swikara082@gmail.com>
2022-08-04 13:57:04 +0545

    feat: 🔮 Opening Document Feature

--- components/docsContent.tsx
+++ components/docsContent.tsx
@@ -4,9 +4,14 @@ const DocsContent = ({ docs }: any) => {
       <h1 className="text-white font-semibold max-w-max overflow-hidden">
         {docs}
       </h1>
-      <button className="bg-tertiary p-5  items-center rounded-lg">
+      <a
+        href={docs}
+        target="_blank"
+        className="bg-tertiary p-5  items-center rounded-lg"
+        rel="noreferrer"
+      >
         <h1 className="text-center text-white font-bold">Open Docs</h1>
-      </button>
+      </a>
     </div>
   );
 };
